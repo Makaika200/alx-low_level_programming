453a7f9c5c9389a4fd6d1212a0fac7c5db18c489
makaika200 <quadriolarewaju200@gmail.com>
2023-09-18 23:16:15 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,20 +1,41 @@
 #include "main.h"
 /**
- * print_number - prints an interger
- * @n: input interger parameter
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from string
  */
-void print_number(int n)
+int _atoi(char *s)
 {
-	unsigned int i = n;
+	int i, d, n, len, f, digit;
 
-	if (n < 0)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
 	{
-		_putchar(45);
-		i - -i;
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+
+	if (f == 0)
+		return (0);
+	return (n);
 }
